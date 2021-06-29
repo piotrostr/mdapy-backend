5198396160caf8901db073ee20f7f34efcea90d9
piotr-ost <piotr.ostrowski@aol.com>
2021-06-29 15:03:26 +0200

    add api cors headers, extract and adjust two mdapy functions

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 import sys
 import json
 
-from MDAPy import MDAPy_Functions as mda
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-
+from mdapy import load_data, validate_analyses_df
+from MDAPy import MDAPy_Functions as mdapy
+from flask import Flask, jsonify, request, make_response
 
 app = Flask(__name__)
 
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
 @app.route('/')
-@cross_origin()
 def index():
     print(mda.__dir__())
     return jsonify('MDAPy API home')
 
-@app.route('/validate', methods=['POST'])
-@cross_origin()
+@app.route('/validate', methods=['POST', 'OPTIONS'])
 def check_validity():
-    data = json.loads(request.data)
-    # for now only return params
-    return jsonify(data)
-
+    if len(request.data):
+        data = json.loads(request.data)
+        # *_, analyses_df = load_data(data['table'])
+        response = make_response(data)
+    else:
+        msg = 'no data could be retrieved from your request'
+        response = make_response(jsonify(msg))
+    methods_allowed = 'GET,HEAD,OPTIONS,POST,PUT'
+    headers_allowed = 'Access-Control-Allow-Headers, Origin, Accept, ' 
+    headers_allowed += 'X-Requested-With, Content-Type, ' 
+    headers_allowed += 'Access-Control-Request-Method, ' 
+    headers_allowed += 'Access-Control-Request-Headers'
+    response.headers['Access-Control-Allow-Origin'] = '*' 
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Allow-Methods'] = methods_allowed
+    response.headers['Access-Control-Allow-Headers'] = headers_allowed
+    response.headers['Content-Type'] = 'application/json'
+    return response
---
+++ mdapy.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+def load_data(arrays, column_names, data_type): 
+    dataSheet = 'Data'
+    ID_col = 'Sample_ID'
+    obj1 = []
+    obj2 = []
+    obj3 = []
+    obj4 = []
+    analyses_df = pd.DataFrame(arrays, columns=column_names)
+    main_df = pd.DataFrame(analyses_df['Sample_ID'].unique(), columns=['Sample_ID'])
+    samples_df = main_df.copy()
+    for sample_ind in range(main_df.shape[0]):
+        active_sample_id = main_df.loc[sample_ind,ID_col]
+        active_UPb_data = dfs[dataSheet].loc[dfs[dataSheet][ID_col].isin([active_sample_id]),:]
+        for colname in active_UPb_data:
+            if colname not in [ID_col]: 
+                if colname in samples_df.columns:
+                    colname_adj = colname+'_'+dataSheet 
+                    if colname_adj not in main_df.columns:
+                        main_df[colname_adj] = (np.nan*np.empty(shape=(len(main_df),1))).tolist()
+                        main_df[colname_adj] = np.asarray(main_df[colname_adj])                
+                    main_df.at[sample_ind,colname_adj] = active_UPb_data[colname].values
+                else:
+                    if colname not in main_df.columns: 
+                        main_df[colname] = (np.nan*np.empty(shape=(len(main_df),1))).tolist()
+                        main_df[colname] = np.asarray(main_df[colname])
+                    main_df.at[sample_ind,colname] = active_UPb_data[colname].values
+                    
+    main_byid_df = main_df.copy()
+    main_byid_df.set_index(ID_col,inplace=True, drop=False)
+    obj1.append(main_df)
+    obj2.append(main_byid_df)
+    obj3.append(samples_df)
+    obj4.append(analyses_df)
+    main_df = pd.concat(obj1, sort=False)
+    main_byid_df = pd.concat(obj2, sort=False)
+    samples_df = pd.concat(obj3, sort=False)
+    analyses_df = pd.concat(obj4, sort=False)
+    return main_df, main_byid_df, samples_df, analyses_df
+
+
+def validate_analyses_df(df: pd.DataFrame, data_type: str) -> pd.DataFrame: 
+    if data_type == 'Ages':
+        amount_of_samples = df.Sample_ID.nunique()
+        sample_array = df["Sample_ID"].unique() 
+        sample_amounts_table = df.groupby('Sample_ID').Best_Age.count().reset_index()
+        sample_amounts_table.rename(columns={'Best_Age': 'Sample_Size'}, inplace=True)
+    if data_type == '238U/206Pb_&_207Pb/206Pb':
+        amount_of_samples = df.Sample_ID.nunique()
+        sample_array = df["Sample_ID"].unique() 
+        df.rename(columns={'238U/206Pb': 'eight_six_U_Pb'}, inplace=True)
+        sample_amounts_table = df.groupby('Sample_ID').eight_six_U_Pb.count().reset_index()
+        sample_amounts_table.rename(columns={'eight_six_U_Pb': 'Sample_Size'}, inplace=True)
+    return sample_amounts_table
